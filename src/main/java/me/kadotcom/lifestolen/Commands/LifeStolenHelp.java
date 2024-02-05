24e90b9c4c03b452fa81ad7d1a11e27ae8659ad7
KadotCom (Kaiden) <76606192+kadotcom@users.noreply.github.com>
2024-02-04 20:45:59 -0500

    Deleted lshelp code for now.

--- src/main/java/me/kadotcom/lifestolen/Commands/LifeStolenHelp.java
+++ src/main/java/me/kadotcom/lifestolen/Commands/LifeStolenHelp.java
@@ -8,6 +8,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.help.HelpTopic;
 
 import java.util.Objects;
 
@@ -22,33 +23,17 @@ public class LifeStolenHelp implements CommandExecutor {
         if (sender instanceof Player){
             Player p = (Player) sender;
             if(!plugin.getConfig().getBoolean("permissions.lshelp.bePermissionBased")){
-                if(args.length >= 1){
-                    System.out.println(args.toString());
-                    if(args[0].equals("1")){
-                        p.sendMessage(ChatColor.RED + "LifeStolen " + ChatColor.RESET + "Commands (1/2):\n/resethp - Reset everyone onlines health\n/health - Shows your health\n/giveheart - Gives you a heart item.\n/sethealth - Sets to a player health.\n/addhealth - Add to a players health");
-                    }else if(args[0].equals("2")){
-                        p.sendMessage(ChatColor.RED + "LifeStolen " + ChatColor.RESET + "Commands (2/2):\n/removehealth - Removes from a players health\n/withdraw - Withdraws a heart from yourself\n/reloadls - Reloads the plugin config\n/lshelp - Shows this message.");
-                    }else{
-                        p.sendMessage(ChatColor.RED + "LifeStolen " + ChatColor.RESET + "Commands (1/2):\n/resethp - Reset everyone onlines health\n/health - Shows your health\n/giveheart - Gives you a heart item.\n/sethealth - Sets to a player health.\n/addhealth - Add to a players health");
-                    }
-                }else {
-                    p.sendMessage(ChatColor.RED + "LifeStolen " + ChatColor.RESET + "Commands (1/2):\n/resethp - Reset everyone onlines health\n/health - Shows your health\n/giveheart - Gives you a heart item.\n/sethealth - Sets to a player health.\n/addhealth - Add to a players health");
-                }
+                /*
+                This is gonna go under a HUGE rework.
+                So I am gonna delete the code in here for now, if I do a tested 5.0 without this done, expect it to be disabled.
+                 */
             }
 
             if(plugin.getConfig().getBoolean("permissions.lshelp.bePermissionBased") && p.hasPermission(plugin.getConfig().getString("permissions.lshelp.permission"))){
-                if(args.length >= 1){
-                    System.out.println(args.toString());
-                    if(args[0].equals("1")){
-                        p.sendMessage(ChatColor.RED + "LifeStolen " + ChatColor.RESET + "Commands (1/2):\n/resethp - Reset everyone onlines health\n/health - Shows your health\n/giveheart - Gives you a heart item.\n/sethealth - Sets to a player health.\n/addhealth - Add to a players health");
-                    }else if(args[0].equals("2")){
-                        p.sendMessage(ChatColor.RED + "LifeStolen " + ChatColor.RESET + "Commands (2/2):\n/removehealth - Removes from a players health\n/withdraw - Withdraws a heart from yourself\n/reloadls - Reloads the plugin config\n/lshelp - Shows this message.");
-                    }else{
-                        p.sendMessage(ChatColor.RED + "LifeStolen " + ChatColor.RESET + "Commands (1/2):\n/resethp - Reset everyone onlines health\n/health - Shows your health\n/giveheart - Gives you a heart item.\n/sethealth - Sets to a player health.\n/addhealth - Add to a players health");
-                    }
-                }else {
-                    p.sendMessage(ChatColor.RED + "LifeStolen " + ChatColor.RESET + "Commands (1/2):\n/resethp - Reset everyone onlines health\n/health - Shows your health\n/giveheart - Gives you a heart item.\n/sethealth - Sets to a player health.\n/addhealth - Add to a players health");
-                }
+                /*
+                This is gonna go under a HUGE rework.
+                So I am gonna delete the code in here for now, if I do a tested 5.0 without this done, expect it to be disabled.
+                 */
             }else if (plugin.getConfig().getBoolean("permissions.lshelp.bePermissionBased") && !p.hasPermission(plugin.getConfig().getString("permissions.lshelp.permission"))){
                 p.sendMessage("§f[§cLifeStolen§f] You don't have permission to use this command.");
             }
