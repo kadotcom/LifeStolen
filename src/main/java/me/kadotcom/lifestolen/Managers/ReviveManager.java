60a0cacb6b190494efc05e5bcfc2bb111365d81f
TheDiscordian <therealdiscordian@gmail.com>
2024-02-11 22:01:29 -0500

    Positive feedback on revive success

--- src/main/java/me/kadotcom/lifestolen/Managers/ReviveManager.java
+++ src/main/java/me/kadotcom/lifestolen/Managers/ReviveManager.java
@@ -46,7 +46,7 @@ public class ReviveManager {
 
                     Bukkit.dispatchCommand(console, command);
                     Bukkit.dispatchCommand(console, command2);
-
+                    caller.sendMessage("You revived " + playerName + ".");
                 } else {
                     caller.sendMessage("Mention a player that is dead.");
                 }
@@ -64,6 +64,8 @@ public class ReviveManager {
                             playerData.setGameMode(GameMode.SURVIVAL);
                             playerData.setLocation(new Location(world, spawn.getX(), spawn.getY(), spawn.getZ()));
                             playerData.save();
+
+                            caller.sendMessage("You revived " + playerName + ".");
                         } else {
                             caller.sendMessage("Mention a player that is dead.");
                         }
