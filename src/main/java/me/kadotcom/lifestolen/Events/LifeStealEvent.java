e7c055ac8d4b0ead0fb71ef82bf54df80d6b442a
KadotCom (Kaiden) <76606192+kadotcom@users.noreply.github.com>
2024-02-17 10:18:37 -0500

    Making it so config can control health

--- src/main/java/me/kadotcom/lifestolen/Events/LifeStealEvent.java
+++ src/main/java/me/kadotcom/lifestolen/Events/LifeStealEvent.java
@@ -137,7 +137,7 @@ public class LifeStealEvent implements Listener {
         user.createUser(event.getPlayer());
 
         if (!event.getPlayer().hasPlayedBefore()) {
-            HealthManager.setMaxHealth(plugin.getConfig().getInt("HP.startHP"), event.getPlayer());
+            HealthManager.setMaxHealth(user.getUserFile().getInt("User.Config.Info.Health"), event.getPlayer());
         }
 
         if(!HTTP.get("https://api.spigotmc.org/legacy/update.php?resource=99220").equalsIgnoreCase(plugin.getDescription().getVersion()) && !plugin.getDescription().getVersion().contains("Tested")){
