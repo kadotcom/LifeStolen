83a765cabc55e5b3faf3503b940c427a930a82b3
TheDiscordian <therealdiscordian@gmail.com>
2024-02-23 15:26:17 -0500

    Revive Command: return after failing bounds check

--- src/main/java/me/kadotcom/lifestolen/Commands/Revive.java
+++ src/main/java/me/kadotcom/lifestolen/Commands/Revive.java
@@ -21,6 +21,7 @@ public class Revive implements CommandExecutor {
             if(p.isOp() || p.hasPermission("lifestolen.revive") || p.hasPermission(plugin.getConfig().getString("permissions.permissionToDoEverything"))){
                 if (args.length < 1) {
                     sender.sendMessage("§f[§c" + plugin.getConfig().getString("translation.serverName") + "§f] Usage: " + plugin.getCommand("revive").getUsage());
+                    return true;
                 }
                 new ReviveManager(plugin).revivePlayer(p, args[0]);
             }else{
